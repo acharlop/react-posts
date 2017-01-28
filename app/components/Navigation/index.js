85ae89bd8845bed4cd158fdf97746ba9e4cf65fa
Avi Charlop <avicharlop@gmail.com>
2017-01-28 16:40:23 -0500

    added navbar

--- app/components/Navigation/index.js
+++ app/components/Navigation/index.js
@@ -8,6 +8,7 @@ import React from 'react';
 
 
 import styles from './styles.css';
+import AppBar from '../AppBar';
 
 function Navigation({ topics, selectTopic }) {
   const topicNodes = topics.map(t => (
@@ -20,7 +21,8 @@ function Navigation({ topics, selectTopic }) {
   ));
   return (
     <div className={styles.navigation}>
-    {topicNodes}
+      <AppBar />
+      {topicNodes}
     </div>
   );
 }
