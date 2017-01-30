4c34740fc1b7e22fde09b1fe5b3c1419c2eec4de
Avi Charlop <avicharlop@gmail.com>
2017-01-29 22:22:27 -0500

    copy login form

--- app/components/LinkForm/index.js
+++ app/components/LinkForm/index.js
@@ -7,12 +7,39 @@
 import React from 'react';
 
 import styles from './styles.css';
+import classNames from 'classnames';
 
 class LinkForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  state = {};
+
   render() {
     return (
       <div className={styles.linkForm}>
-        Link Form
+        <div className={styles.heading}>Add a link</div>
+        <input
+          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+          placeholder="URL"
+          ref={(f) => { this.urlField = f; }}
+          type="text"
+        />
+
+        <input
+          className={classNames(styles.input, { [styles.inputError]: this.state.errorText })}
+          placeholder="Description"
+          ref={(f) => { this.descriptionField = f; }}
+          type="text"
+        />
+
+        <div className={styles.actionContainer}>
+          <div
+            className={styles.button}
+          >cancel</div>
+
+          <div
+            className={styles.button}
+            onClick={this.login}
+          >login</div>
+        </div>
       </div>
     );
   }
