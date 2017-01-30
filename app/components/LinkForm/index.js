a4e0c7ee3e2ee081b4a8ef481ccec585b051ba7a
Avi Charlop <avicharlop@gmail.com>
2017-01-29 22:15:44 -0500

    create form component and container

---
+++ app/components/LinkForm/index.js
@@ -0,0 +1,21 @@
+/**
+*
+* LinkForm
+*
+*/
+
+import React from 'react';
+
+import styles from './styles.css';
+
+class LinkForm extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div className={styles.linkForm}>
+        Link Form
+      </div>
+    );
+  }
+}
+
+export default LinkForm;
